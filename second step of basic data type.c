19ca46f7d35f79950d82198567186eb0b5d2cb03
20MH5A0332 <92074917+20MH5A0332@users.noreply.github.com>
2021-10-23 10:57:58 +0530

    Add files via upload

---
+++ second step of basic data type.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main ()
+{
+	//variavble declation
+	float number1,number2,sum;
+	
+	//initialization
+	number1=5.2;
+	number2=88.5;
+	
+	//calculation
+	sum =number1+number2;
+	
+	//output
+	printf("sum of %f and %f is %f",number1,number2,sum);
+}
