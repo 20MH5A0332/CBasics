01effdcd00167d0de6e7033ce022969f95297e46
20MH5A0332 <92074917+20MH5A0332@users.noreply.github.com>
2021-10-23 10:16:20 +0530

    Add files via upload

---
+++ basic datatype.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age = 19;
+	cgpa =6.0;
+	initial = 'b';
+	
+	//output
+	printf("age of student is %d\n",age);
+	printf("cgpa of the student is %f\n",cgpa);
+	printf("student surname start with %c",initial);
+}
